34c195ac1d35793d40abd28ff5955bc19d357aef
Easimer <easimer@outlook.com>
2013-07-23 17:38:38 +0200

    Force language

--- src.cs
+++ src.cs
@@ -1,3 +1,6 @@
+/// Author: Easimer
+/// Date: 2013. 07. 23.
+/// Stage: Alpha
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,56 +23,47 @@ namespace STCF
 
         static SteamUser steamUser;
         static SteamFriends steamFriends;
-
+        static string logo = "..####...######...####...........######...####...#####...##...##..######..#####..\n.##........##....##..##..........##......##..##..##..##..###.###..##......##..##.\n..####.....##....##..............####....######..#####...##.#.##..####....#####..\n.....##....##....##..##..........##......##..##..##..##..##...##..##......##..##.\n..####.....##.....####...........##......##..##..##..##..##...##..######..##..##.\n"; //lel
         static bool isRunning;
 
         static string user, pass;
         static string authCode;
         static void Main(string[] args)
         {
-            //Logo :DD
-            Console.Write("..####...######...####...........######...####...#####...##...##..######..#####..\n.##........##....##..##..........##......##..##..##..##..###.###..##......##..##.\n..####.....##....##..............####....######..#####...##.#.##..####....#####..\n.....##....##....##..##..........##......##..##..##..##..##...##..##......##..##.\n..####.....##.....####...........##......##..##..##..##..##...##..######..##..##.\n");
+            if (args.Length == 1)
+            {
+                AppLocalization.SetLang(args[0].ToString());
+            }
+            else
+            {
+                AppLocalization.SetLang();
+            }
+            Console.Write(logo);
             if (!EasimerNet.CheckForInternetConnection())
             {
-                EasimerNet.WriteError("No internet connection!");
+                EasimerNet.WriteError(AppLocalization.noNetConn);
             }
-            Console.WriteLine("Enter username");
+            Console.WriteLine(AppLocalization.enterUser);
             user = Console.ReadLine();
-            Console.WriteLine("Enter password");
+            Console.WriteLine(AppLocalization.enterPass);
             pass = Console.ReadLine();
-            // create our steamclient instance
             steamClient = new SteamClient();
-            // create the callback manager which will route callbacks to function calls
             manager = new CallbackManager(steamClient);
-
-            // get the steamuser handler, which is used for logging on after successfully connecting
             steamUser = steamClient.GetHandler<SteamUser>();
-            // get the steam friends handler, which is used for interacting with friends on the network after logging on
             steamFriends = steamClient.GetHandler<SteamFriends>();
-
-            // register a few callbacks we're interested in
-            // these are registered upon creation to a callback manager, which will then route the callbacks
-            // to the functions specified
             new Callback<SteamClient.ConnectedCallback>(OnConnected, manager);
             new Callback<SteamClient.DisconnectedCallback>(OnDisconnected, manager);
-
             new Callback<SteamUser.LoggedOnCallback>(OnLoggedOn, manager);
             new Callback<SteamUser.LoggedOffCallback>(OnLoggedOff, manager);
-
-            // this callback is triggered when the steam servers wish for the client to store the sentry file
             new JobCallback<SteamUser.UpdateMachineAuthCallback>(OnMachineAuth, manager);
 
             isRunning = true;
 
-            EasimerNet.WriteInfo("Connecting to Steam...");
-
-            // initiate the connection
+            EasimerNet.WriteInfo(AppLocalization.conn2Steam);
             steamClient.Connect();
             steamFriends.SetPersonaState(EPersonaState.Online);
-            // create our callback handling loop
             while (isRunning)
             {
-                // in order for the callbacks to get routed, they need to be handled by the manager
                 manager.RunWaitCallbacks(TimeSpan.FromSeconds(1));
             }
             
@@ -78,18 +72,17 @@ namespace STCF
         {
             if (callback.Result != EResult.OK)
             {
-                Console.WriteLine("Unable to connect to Steam: {0}", callback.Result);
+                Console.WriteLine(AppLocalization.unable2Conn, callback.Result);
 
                 isRunning = false;
                 return;
             }
 
-            Console.WriteLine("Connected to Steam! Logging in '{0}'...", user);
+            Console.WriteLine(AppLocalization.connLog, user);
 
             byte[] sentryHash = null;
             if (File.Exists("sentry.bin"))
             {
-                // if we have a saved sentry file, read and sha-1 hash it
                 byte[] sentryFile = File.ReadAllBytes("sentry.bin");
                 sentryHash = CryptoHelper.SHAHash(sentryFile);
             }
@@ -98,23 +91,14 @@ namespace STCF
             {
                 Username = user,
                 Password = pass,
-
-                // in this sample, we pass in an additional authcode
-                // this value will be null (which is the default) for our first logon attempt
                 AuthCode = authCode,
-
-                // our subsequent logons use the hash of the sentry file as proof of ownership of the file
-                // this will also be null for our first (no authcode) and second (authcode only) logon attempts
                 SentryFileHash = sentryHash,
             });
         }
 
         static void OnDisconnected(SteamClient.DisconnectedCallback callback)
         {
-            // after recieving an AccountLogonDenied, we'll be disconnected from steam
-            // so after we read an authcode from the user, we need to reconnect to begin the logon flow again
-
-            EasimerNet.WriteWarning("Disconnected from Steam, reconnecting in 5...");
+            EasimerNet.WriteWarning(AppLocalization.reConn);
 
             Thread.Sleep(TimeSpan.FromSeconds(5));
 
@@ -125,8 +109,8 @@ namespace STCF
         {
             if (callback.Result == EResult.AccountLogonDenied)
             {
-                EasimerNet.WriteInfo("This account is SteamGuard protected!");
-                Console.Write("Please enter the auth code sent to the email at {0}: ", callback.EmailDomain);
+                EasimerNet.WriteInfo(AppLocalization.steamGuard);
+                Console.Write(AppLocalization.enterKey, callback.EmailDomain);
 
                 authCode = Console.ReadLine();
                 return;
@@ -134,35 +118,27 @@ namespace STCF
 
             if (callback.Result != EResult.OK)
             {
-                Console.WriteLine("Unable to logon to Steam: {0} / {1}", callback.Result, callback.ExtendedResult);
+                Console.WriteLine(AppLocalization.unable2Log, callback.Result, callback.ExtendedResult);
 
                 isRunning = false;
                 return;
             }
 
-            EasimerNet.WriteInfo("Successfully logged on!");
+            EasimerNet.WriteInfo(AppLocalization.successLog);
 
             GetCMD();
         }
 
         static void OnLoggedOff(SteamUser.LoggedOffCallback callback)
         {
-            Console.WriteLine("Logged off of Steam: {0}", callback.Result);
+            Console.WriteLine(AppLocalization.logOff, callback.Result);
         }
 
         static void OnMachineAuth(SteamUser.UpdateMachineAuthCallback callback, JobID jobId)
         {
-            EasimerNet.WriteInfo("Updating sentryfile...");
-
+            EasimerNet.WriteInfo(AppLocalization.sentry);
             byte[] sentryHash = CryptoHelper.SHAHash(callback.Data);
-
-            // write out our sentry file
-            // ideally we'd want to write to the filename specified in the callback
-            // but then this sample would require more code to find the correct sentry file to read during logon
-            // for the sake of simplicity, we'll just use "sentry.bin"
             File.WriteAllBytes("sentry.bin", callback.Data);
-
-            // inform the steam servers that we're accepting this sentry file
             steamUser.SendMachineAuthResponse(new SteamUser.MachineAuthDetails
             {
                 JobID = jobId,
@@ -181,7 +157,7 @@ namespace STCF
                 SentryFileHash = sentryHash,
             });
 
-            EasimerNet.WriteInfo("Done!");
+            EasimerNet.WriteInfo(AppLocalization.done);
         }
         /// <summary>
         /// Launch a game
@@ -212,7 +188,7 @@ namespace STCF
             string tempcmd = Console.ReadLine();
             if (tempcmd.Contains("LaunchGame()"))
             {
-                Console.WriteLine("Enter a game appid (ex.: 420 = TF2, 730 = CS:GO)");
+                Console.WriteLine(AppLocalization.enterAppID);
                 LaunchGame(Convert.ToUInt64(Console.ReadLine()));
                 GetCMD();
                 return 1;
@@ -220,20 +196,20 @@ namespace STCF
             else if (tempcmd == "GoOnline()")
             {
                 string profileName = steamFriends.GetPersonaName();
-                EasimerNet.WriteInfo(profileName + " is now Online!");
+                EasimerNet.WriteInfo(profileName + AppLocalization.isOnline);
                 GoOnline();
                 GetCMD();
                 return 1;
             }
             else if (tempcmd == "Exit()")
             {
-                Console.WriteLine("Beep Boop Shutting Down...");
+                Console.WriteLine(AppLocalization.beepBoop);
                 Environment.Exit(0);
                 return 0;
             }
             else
             {
-                EasimerNet.WriteError("Invalid command");
+                EasimerNet.WriteError(AppLocalization.invalidCmd);
                 GetCMD();
                 return 0;
             }
@@ -256,7 +232,7 @@ namespace STCF
             }
             try
             {
-                Console.WriteLine("Checking for internet connection...");
+                Console.WriteLine(AppLocalization.check4Net);
                 using (var client = new WebClient())
                 using (var stream = client.OpenRead(url))
                 {
@@ -275,7 +251,7 @@ namespace STCF
         public static void WriteError(string text)
         {
             Console.BackgroundColor = ConsoleColor.Red;
-            Console.WriteLine("[ERROR] " + text);
+            Console.WriteLine(AppLocalization.error + text);
             Console.BackgroundColor = ConsoleColor.Black;
         }
         /// <summary>
@@ -285,7 +261,7 @@ namespace STCF
         public static void WriteWarning(string text)
         {
             Console.BackgroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("[WARNING] " + text);
+            Console.WriteLine(AppLocalization.warning + text);
             Console.BackgroundColor = ConsoleColor.Black;
         }
         /// <summary>
@@ -295,15 +271,106 @@ namespace STCF
         public static void WriteInfo(string text)
         {
             Console.BackgroundColor = ConsoleColor.Blue;
-            Console.WriteLine("[INFO] " + text);
+            Console.WriteLine(AppLocalization.info + text);
             Console.BackgroundColor = ConsoleColor.Black;
         }
-        /// <summary>
-        /// Beep Boop
-        /// </summary>
-        public static void Beep()
+        public static string GetOSLang()
         {
-            Console.Beep();
+            string lang = System.Globalization.CultureInfo.CurrentCulture.ToString();
+            return lang;
+        }
+    }
+    class AppLocalization
+    {
+        //fuck the static
+        public static string noNetConn,enterUser,enterPass,conn2Steam,unable2Conn,connLog,reConn,steamGuard,enterKey,unable2Log,successLog,logOff,sentry,done,enterAppID,isOnline,check4Net,error, info, warning,beepBoop,invalidCmd;
+        public static void SetLang(string lang = null)
+        {
+            if (lang == null)
+            {
+                string curlang = EasimerNet.GetOSLang();
+            }
+            else
+            {
+
+            }
+
+        if (lang=="hu") //hungarian
+        {
+            noNetConn = "Nincs internet-kapcsolat!";
+            enterUser = "Felhasználónév:";
+            enterPass = "Jelszó:";
+            conn2Steam = "Kapcsolódás a Steam-hez...";
+            unable2Conn = "Sikertelen kapcsolódás: {0}";
+            connLog = "Kapcsolódva a Steam-hez. Bejelentkezés mint '{0}'...";
+            reConn = "Elveszett a kapcsolat a Steam-el, újrakapcsolódás...";
+            steamGuard = "Ezt a profilt a Steam Guard védi!";
+            enterKey = "Írd be a {0}-ra/re küldött kódot: ";
+            unable2Log = "Sikertelen bejelentkezés: {0} / {1}";
+            successLog = "Sikeres bejelentkezés!";
+            logOff = "Kilépés a Steam-ről: {0}";
+            sentry = "Sentry-fájl frissítése...";
+            done = "Kész.";
+            enterAppID = "Írj be egy játék ID-t (pl.: 420 = TF2, 730 = CS:GO)";
+            isOnline = " Online lett!";
+            check4Net = "Internet kapcsolat keresése...";
+            error = "[HIBA] ";
+            warning = "[FIGYELMEZTETÉS] ";
+            info = "[INFÓ] ";
+            invalidCmd = "Érvénytelen parancs";
+            beepBoop = "Bip Bip Leállítás...";
+        }
+        else if (lang == "fr") //french
+        {
+            noNetConn = "Pas de connexion internet!";
+            enterUser = "Nom d'utilisateur:";
+            enterPass = "Mot de passe:";
+            conn2Steam = "Connectiong à Steam...";
+            unable2Conn = "Impossible de se connecter à la Steam: {0}";
+            connLog = "Connecté à Steam! Connexion en tant '{0}'...";
+            reConn = "Déconnecté de Steam, reconnectant...";
+            steamGuard = "Ce compte est protégé par SteamGuard!";
+            enterKey = "Veuillez saisir le code Steam Guard envoyé à votre adresse email: {0} ";
+            unable2Log = "Impossible de se connecter à Steam: {0} / {1}";
+            successLog = "Réussir connecté!";
+            logOff = "Déconnecté de Steam: {0}";
+            sentry = "Mise à jour de sentryfile...";
+            done = "Fait!";
+            enterAppID = "Entrez un ID de jeu (ex.: 420 = TF2, 730 = CS:GO)";
+            isOnline = " est en ligne!";
+            check4Net = "Vérification de la connexion internet...";
+            error = "[ERREUR] ";
+            warning = "[ALERTE] ";
+            info = "[INFOS] ";
+            invalidCmd = "Commande invalide";
+            beepBoop = "Beep Boop Arrêt...";
+        }
+        else //default (english)
+        {
+            noNetConn = "No internet connection!";
+            enterUser = "Enter username:";
+            enterPass = "Enter password:";
+            conn2Steam = "Connecting to Steam...";
+            unable2Conn = "Unable to connect to Steam: {0}";
+            connLog = "Connected to Steam! Logging in '{0}'...";
+            reConn = "Disconnected from Steam, reconnecting in 5...";
+            steamGuard = "This account is SteamGuard protected!";
+            enterKey = "Please enter the auth code sent to the email at {0}: ";
+            unable2Log = "Unable to logon to Steam: {0} / {1}";
+            successLog = "Successfully logged on!";
+            logOff = "Logged off of Steam: {0}";
+            sentry = "Updating sentryfile...";
+            done = "Done.";
+            enterAppID = "Enter a game appid (ex.: 420 = TF2, 730 = CS:GO)";
+            isOnline = " is now Online!";
+            check4Net = "Checking for internet connection...";
+            error = "[ERROR] ";
+            warning = "[WARNING] ";
+            info = "[INFO] ";
+            invalidCmd = "Invalid command";
+            beepBoop = "Beep Boop Shutting Down...";
+        }
         }
     }
 }
+//After over 4 hours in development, hopefully it would have been worth the weight
